fd9c41b449d58d03b2b22c6b664b641e5569bc3d
Valentina <mvmontilva_usistemas@intep.edu.co>
2025-08-19 15:01:54 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+numero1 = 1
+numero2 = 2
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicación = numero1 * numero2
+división = numero1 / numero2
+print(suma) 
+print(resta)
+print(multiplicación)
+print(división)
